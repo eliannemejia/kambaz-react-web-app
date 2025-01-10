28876f763a7c09b234051a675c5c16c09e372eab
eliannemejia <125212380+eliannemejia@users.noreply.github.com>
2025-01-09 20:13:29 -0500

    account screen has working navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import Labs from "./Labs";
-
+import Kambaz from "./Kambaz";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
   return (
-  <div>
-    <Labs />
-  </div>
+    <HashRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="Kambaz" />} />
+          <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kambaz/*" element={<Kambaz />} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
