1b4ea3d9dcde436c2e91118d87d8ff9e3ed003d3
eliannemejia <eliannemejia04@gmail.com>
2025-04-12 12:11:58 -0400

    first a6 commit

--- src/Kambaz/Account/index.tsx
+++ src/Kambaz/Account/index.tsx
@@ -4,6 +4,7 @@ import Profile from "./Profile";
 import Signin from "./Signin";
 import Signup from "./Signup";
 import { useSelector } from "react-redux";
+import Users from "./Users";
 
 export default function Account() {
     const { currentUser } = useSelector((state: any) => state.accountReducer);
@@ -17,10 +18,12 @@ export default function Account() {
                 </div>
                 <div className="wd-margin-left">
                     <Routes>
-                        <Route path="/" element={<Navigate to={ currentUser ? "/Kambaz/Account/Profile" : "/Kambaz/Account/Signin" } />} />
+                        <Route path="/" element={<Navigate to={currentUser ? "/Kambaz/Account/Profile" : "/Kambaz/Account/Signin"} />} />
                         <Route path="Signin" element={<Signin />} />
                         <Route path="Profile" element={<Profile />} />
                         <Route path="Signup" element={<Signup />} />
+                        <Route path="/Users" element={<Users />} />
+                        <Route path="/Users/:uid" element={<Users />} />
                     </Routes>
                 </div>
             </div>
